11338004ead59d1004833103894ac83054394d0e
Unknown <unknown@example.com>
2018-03-23 14:12:05 +1100

    add base structure for map component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
+import Map from './Map.js'
 import './App.css';
 import * as Strings_en from './Strings_en.js';
 
@@ -11,9 +12,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
             <h3>{Strings_en.COUNCIL_FULL_NAME}</h3>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. Done!
-        </p>
+        {/*<p className="App-intro">*/}
+          {/*To get started, edit <code>src/App.js</code> and save to reload. Done!*/}
+        {/*</p>*/}
+        <Map />
           <footer className="App-footer">
               <p>{Strings_en.FOOTER_CREDIT} </p>
           </footer>
